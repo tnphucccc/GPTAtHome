29a627de31cc23f506c26856066a4e08c3c9d58a
tnphucccc <ktsukasa1412@gmail.com>
2025-01-07 12:38:49 +0000

    chores: remove unused file, update data source

--- src/utils/data_processor.py
+++ src/utils/data_processor.py
@@ -1,8 +1,18 @@
-import torch
-
+# Using the following command to download the input.txt file:
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
 class TextProcessor:
+    """
+    Class for encoding and decoding text data.
+    """
+
     def __init__(self, file_path):
+        """
+        Initialize the TextProcessor.
+
+        Args:
+            file_path (str): Path to the text file to process
+        """
         with open(file_path, 'r', encoding='utf-8') as f:
             self.text = f.read()
         self.chars = sorted(list(set(self.text)))
@@ -11,7 +21,19 @@ class TextProcessor:
         self.itos = {i: ch for i, ch in enumerate(self.chars)}
 
     def encode(self, s):
+        """
+        Encode a string into a list of integers.
+
+        Args:
+            s (str): Input string to encode
+        """
         return [self.stoi[c] for c in s]
 
     def decode(self, l):
+        """
+        Decode a list of integers into a string.
+
+        Args:
+            l (list): Input list of integers to decode
+        """
         return ''.join([self.itos[i] for i in l])
